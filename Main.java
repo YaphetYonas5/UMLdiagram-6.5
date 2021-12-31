756ea312bcf7c0a9347631f58f2b928b6cf600c4
YaphetYonas1 <85519133+YaphetYonas5@users.noreply.github.com>
2021-12-31 03:36:44 +0000

    Initial commit

---
+++ Main.java
@@ -0,0 +1,21 @@
+class Main {
+  public static void main(String[] args) {
+    System.out.println("Layout of a UML diagram for a class");
+System.out.println(" ");
+System.out.println("The top box in the UML diagram contains the name of the class.");
+System.out.println(" ");
+System.out.println("The middle box contains the data members and their data types.");
+System.out.println(" ");
+System.out.println("The last box contains the method names, parameter list, and return types.");
+System.out.println(" ");
+System.out.println("To indicate the data type of a field, place a colon followed by the name of the data type after the name of the field.");System.out.println(" ");
+System.out.println("To indicate return type of method place a colon followed by the method name.");System.out.println(" ");
+System.out.println("Parameters and their data types inside a method's parentheses.");
+System.out.println(" ");
+System.out.println("The + (plus) sign in front of a member indicates that it is a public member.");
+System.out.println(" ");
+System.out.println("The – (minus) sign indicates that it is a private member.");
+System.out.println(" ");
+System.out.println("The # symbol before a member name indicates that it is a protected member.");
+  }
+}
